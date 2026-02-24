fa621875fe374598ce65e4d87bbf4920451e54fa
arkteck <arkteck@gmail.com>
2026-02-24 12:30:38 -0500

    state

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
 use std::collections::HashMap;
+use std::sync::Arc;
 
-use axum::extract::{Path, Query};
+use axum::extract::{Path, Query, State};
 use axum::http::HeaderMap;
 use axum::{Router, response::Html, routing::get};
 
+struct MyConfig {
+    config_string: String,
+}
+
 #[tokio::main]
 async fn main() {
+    let shared_config = Arc::new(MyConfig {
+        config_string: "My config string".to_string(),
+    });
+
     let app = Router::new()
         .route("/", get(handler))
         .route("/book/{id}", get(path_extract))
         .route("/book", get(query_extract))
-        .route("/header", get(header_extract));
+        .route("/header", get(header_extract))
+        .with_state(shared_config);
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3001")
         .await
@@ -20,8 +30,8 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn handler() -> Html<&'static str> {
-    Html("<h1>Hello World</h1>")
+async fn handler(State(config): State<Arc<MyConfig>>) -> Html<String> {
+    Html(format!("<h1>{}</h1>", config.config_string))
 }
 
 async fn path_extract(Path(id): Path<u32>) -> Html<String> {
