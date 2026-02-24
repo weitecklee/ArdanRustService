51c2986f3c34b3c07dbdc15c4bc53f09d0fe24cb
arkteck <arkteck@gmail.com>
2026-02-24 13:53:13 -0500

    multiple states with layers

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,27 @@ use std::collections::HashMap;
 use std::sync::Arc;
 use std::sync::atomic::AtomicUsize;
 
-use axum::extract::{Path, Query, State};
+use axum::Extension;
+use axum::extract::{Path, Query};
 use axum::http::HeaderMap;
 use axum::{Router, response::Html, routing::get};
 
-struct MyConfig {
+struct MyCounter {
     counter: AtomicUsize,
+}
+
+struct MyConfig {
+    text: String,
 }
 
 #[tokio::main]
 async fn main() {
-    let shared_config = Arc::new(MyConfig {
+    let shared_counter = Arc::new(MyCounter {
         counter: AtomicUsize::new(0),
+    });
+
+    let shared_text = Arc::new(MyConfig {
+        text: "This is my configuration.".to_string(),
     });
 
     let app = Router::new()
@@ -21,7 +30,8 @@ async fn main() {
         .route("/book/{id}", get(path_extract))
         .route("/book", get(query_extract))
         .route("/header", get(header_extract))
-        .with_state(shared_config);
+        .layer(Extension(shared_counter))
+        .layer(Extension(shared_text));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3001")
         .await
@@ -31,13 +41,17 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn handler(State(config): State<Arc<MyConfig>>) -> Html<String> {
-    config
+async fn handler(
+    Extension(counter): Extension<Arc<MyCounter>>,
+    Extension(config): Extension<Arc<MyConfig>>,
+) -> Html<String> {
+    counter
         .counter
         .fetch_add(1, std::sync::atomic::Ordering::Relaxed);
     Html(format!(
-        "<h1>You are visitor number {}!</h1>",
-        config.counter.load(std::sync::atomic::Ordering::Relaxed)
+        "<h1>{} You are visitor number {}!</h1>",
+        config.text,
+        counter.counter.load(std::sync::atomic::Ordering::Relaxed)
     ))
 }
 
