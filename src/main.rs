a594d0781705ed660a212f882fab5e602fe6aaf5
arkteck <arkteck@gmail.com>
2026-02-24 10:13:16 -0500

    setting up http server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, response::Html, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(handler));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3001")
+        .await
+        .unwrap();
+
+    println!("Listening on 127.0.0.1:3001");
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1>Hello World</h1>")
 }
