9ae1ad068f548f59748dde4d6210ed1ca5a77da3
arkteck <arkteck@gmail.com>
2026-02-24 10:43:09 -0500

    query extraction

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-use axum::extract::Path;
+use std::collections::HashMap;
+
+use axum::extract::{Path, Query};
 use axum::{Router, response::Html, routing::get};
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
         .route("/", get(handler))
-        .route("/book/{id}", get(path_extract));
+        .route("/book/{id}", get(path_extract))
+        .route("/book", get(query_extract));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3001")
         .await
@@ -21,4 +24,8 @@ async fn handler() -> Html<&'static str> {
 
 async fn path_extract(Path(id): Path<u32>) -> Html<String> {
     Html(format!("Hello, {id}!"))
+}
+
+async fn query_extract(Query(params): Query<HashMap<String, String>>) -> Html<String> {
+    Html(format!("{params:#?}"))
 }
