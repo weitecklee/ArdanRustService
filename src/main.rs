703a00b866906d541a913382a644e175ec041f4d
arkteck <arkteck@gmail.com>
2026-02-24 10:31:32 -0500

    path extraction

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use axum::extract::Path;
 use axum::{Router, response::Html, routing::get};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(handler));
+    let app = Router::new()
+        .route("/", get(handler))
+        .route("/book/{id}", get(path_extract));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3001")
         .await
@@ -14,4 +17,8 @@ async fn main() {
 
 async fn handler() -> Html<&'static str> {
     Html("<h1>Hello World</h1>")
+}
+
+async fn path_extract(Path(id): Path<u32>) -> Html<String> {
+    Html(format!("Hello, {id}!"))
 }
